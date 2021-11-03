110374c7203db54133f3d534ba541e4bd3a8988d
ksasakipz <ksasaki@students.pitzer.edu>
2021-11-03 01:15:12 -0700

    Cleaned up analyze_characters.py a tad

--- analyze_characters.py
+++ analyze_characters.py
@@ -1,23 +1,33 @@
-def main():
+def analyze(in_file_name, in_permissions, out_permission):
     print("Hello there")
-    file1 = open('DONTREADME.md', 'r')
+    print()
+    in_file = open(in_file_name, in_permissions)
+    out_file = open('out.' + in_file_name, out_permission)
     line_count = 0
+    cumulative_char_count = 0
     char_count = 0
      
     while True:
         line_count += 1
      
         # Get next line from file
-        line = file1.readline()
+        line = in_file.readline()
      
         # if line is empty
         # end of file is reached
         if not line:
             break
-        char_count += len(line)
-        print("Line {} char {}: {}".format(line_count, len(line), line.strip()))
-     
-    file1.close()
+        cumulative_char_count += len(line)
+        char_count = len(line)
+        out_file.write("Line {} char {}: {}".format(line_count, char_count, line))
+    in_file.close()
+    out_file.close()
+
+def main():
+    analyze('DONTREADME.md', 'rb', 'a')
+    analyze('DONTREADME.md', 'r', 'a')
+    analyze('out.DONTREADME.md', 'rb', 'a')
+    analyze('out.DONTREADME.md', 'r', 'a')
 
 if __name__ == "__main__":
     main()
