b6103c2c21af044600f89151024fc6d760e704bc
ksasakipz <ksasaki@students.pitzer.edu>
2021-11-03 09:47:11 -0700

    Added comments, global variables, exception boundaries to analyze_characters.py and out* .gitignore

--- analyze_characters.py
+++ analyze_characters.py
@@ -1,5 +1,71 @@
+# Magic Numbers
+DESTINATION = 10
+
+# Variables Used
+"""
+"""
+
+# Functions Used
+"""
+"""
+
+# Key Words Used
+"""
+"""
+
+# Punctuation Used
+"""
+"""
+
+# Whitespace Encoded
+"""
+"""
+
+# Code Encoded
+
+# Code Decoded
+
+def kprint(payload, destination):
+    print(payload, DESTINATION)
+    print(payload, destination)
+    print()
+    increment_destination()
+    print(payload, DESTINATION)
+    print(payload, destination)
+    print()
+    if (destination == 0):
+        print(payload, DESTINATION)
+    elif (destination == 1):
+        pass
+    elif (destination == 2):
+        pass
+    elif (destination == 3):
+        pass
+    elif (destination == 4):
+        pass
+    elif (destination == 5):
+        pass
+    elif (destination == 6):
+        pass
+    elif (destination == 7):
+        pass
+    elif (destination == 8):
+        pass
+    elif (destination == 9):
+        pass
+    elif destination == 10:
+        raise(Exception(payload))
+    else:
+        raise(BaseException(payload))
+
+def increment_destination():
+    global DESTINATION
+    DESTINATION += 1
+    if DESTINATION > 10:
+        DESTINATION = 0
+
 def analyze(in_file_name, in_permissions, out_permission):
-    print("Hello there")
+    kprint("Hello there " + in_file_name + " " + in_permissions, DESTINATION)
     print()
     in_file = open(in_file_name, in_permissions)
     out_file = open('out.' + in_file_name, out_permission)
@@ -30,5 +96,6 @@ def main():
     analyze('out.DONTREADME.md', 'r', 'a')
 
 if __name__ == "__main__":
+    DESTINATION = 0
     main()
 
