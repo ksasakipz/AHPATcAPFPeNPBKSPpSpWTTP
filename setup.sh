cb1745673d4a27ba49244601ca3f6278ecdfc423
ksasakipz <ksasaki@students.pitzer.edu>
2021-11-04 17:27:57 -0700

    Added The-Game to setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
 git clone https://github.com/ksasakipz/dot.git
 git clone https://github.com/ksasakipz/garage.git
 git clone https://github.com/ksasakipz/toolbox.git
+git clone https://github.com/ksasakipz/The-Game.git
 git clone https://gitlab.com/ksasakipz/hermes
