0fa0444e14c2ec1d0cb44cbd5d2034fcc09bd8ec
ksasakipz <ksasaki@students.pitzer.edu>
2021-11-04 17:34:05 -0700

    "Added the library. Sorted my repositories in setup.sh"

--- setup.sh
+++ setup.sh
@@ -2,4 +2,5 @@ git clone https://github.com/ksasakipz/dot.git
 git clone https://github.com/ksasakipz/garage.git
 git clone https://github.com/ksasakipz/toolbox.git
 git clone https://github.com/ksasakipz/The-Game.git
+git clone https://github.com/ksasakipz/library.git
 git clone https://gitlab.com/ksasakipz/hermes
