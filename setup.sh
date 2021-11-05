ccf2c1bd8a7b6e621f904ca0eea1c7249098180a
ksasakipz <ksasaki@students.pitzer.edu>
2021-11-04 17:20:38 -0700

    "Added hermes to .gitignore and initialize setup.sh"

---
+++ setup.sh
@@ -0,0 +1,4 @@
+git clone https://github.com/ksasakipz/dot.git
+git clone https://github.com/ksasakipz/garage.git
+git clone https://github.com/ksasakipz/toolbox.git
+git clone https://gitlab.com/ksasakipz/hermes
