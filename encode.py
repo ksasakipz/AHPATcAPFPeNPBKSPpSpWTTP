052c377da77e8ad7c98d14da42fdbfa1ca4de11e
ksasakipz <kevinmsasaki@gmail.com>
2021-12-19 02:29:14 -0800

    ksasakipz.github.io/p=np/a.jpg

--- encode.py
+++ encode.py
@@ -1,1 +1,33 @@
+import random
 print(len("10000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000"))
+
+def factorial(n):
+    if n < 2:
+        return 1
+    elif n == 2:
+        return 2
+    else:
+        return factorial(n-1) * n
+
+
+
+def main():
+    retval = ""
+    index = 0
+    while True:
+        retval += str(random.randrange(factorial(index)))
+
+        if len(retval) > 80:
+            index += 1
+            print(f'index: {index} | retval: {retval}')
+            retval = ""
+
+        elif len(retval) < 80:
+            print()
+            pass
+        else:
+            index -= 1
+            
+
+if __name__ == "__main__":
+    main()
