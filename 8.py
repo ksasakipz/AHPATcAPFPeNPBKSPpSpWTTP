90c28064f423a2ca2acbd9188351fb42b80c13d4
ksasakipz <kevinmsasaki@gmail.com>
2022-01-04 18:04:11 -0800

    trying to delete a file

--- 8.py
+++ 8.py
@@ -1,7 +1,15 @@
+import subprocess
+import sys
 import os
+
+prefix = "/Users/kevinsasaki1/")
 
 if __name__ == "__main__":
     print("Hello to Kevin's Macbook pro")
-    x = os.listdir("/Users/kevinsasaki1/")
+    x = os.listdir(prefix)
     print('os.listdir(~/): ' + str(x))
+
+    sprocess = subprocess.cal([f"rm {prefix}open.spotify.com:track:6tn96Btxy5ysJADC6cXGz8.webloc"])
+
+    
     
