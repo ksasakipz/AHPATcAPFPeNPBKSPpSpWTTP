a67fe25470a1594ad75330484323e32aee1a96cb
ksasakipz <kevinmsasaki@gmail.com>
2022-01-04 17:59:46 -0800

    trying new directory

--- 8.py
+++ 8.py
@@ -2,6 +2,6 @@ import os
 
 if __name__ == "__main__":
     print("Hello to Kevin's Macbook pro")
-    x = os.listdir("~/")
+    x = os.listdir("/Users/kevinsasaki/")
     print('os.listdir(~/): ' + x)
     
