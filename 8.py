e5ca3752ac5fb7a1c25def275cc0baa7b0886c8a
ksasakipz <kevinmsasaki@gmail.com>
2022-01-04 18:00:48 -0800

    syntax again

--- 8.py
+++ 8.py
@@ -3,5 +3,5 @@ import os
 if __name__ == "__main__":
     print("Hello to Kevin's Macbook pro")
     x = os.listdir("/Users/kevinsasaki/")
-    print('os.listdir(~/): ' + x)
+    print('os.listdir(~/): ' + str(x))
     
