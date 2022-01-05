d4a4c64676df39cce229a8850802fb4eb6f46226
ksasakipz <kevinmsasaki@gmail.com>
2022-01-04 18:01:34 -0800

    typo

--- 8.py
+++ 8.py
@@ -2,6 +2,6 @@ import os
 
 if __name__ == "__main__":
     print("Hello to Kevin's Macbook pro")
-    x = os.listdir("/Users/kevinsasaki/")
+    x = os.listdir("/Users/kevinsasaki1/")
     print('os.listdir(~/): ' + str(x))
     
