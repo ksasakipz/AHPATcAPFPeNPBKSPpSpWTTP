3dd1c339644c655229c7cdd156e1a88d8e30d86b
ksasakipz <kevinmsasaki@gmail.com>
2022-01-07 21:45:36 -0800

    2022-01-07 21:45:36.262074

---
+++ 9.py
@@ -0,0 +1,18 @@
+import subprocess
+import sys
+import os
+
+prefix = "/Users/kevinsasaki/sandbox"
+
+if __name__ == "__main__":
+    print("Hello to Kevin's Macbook pro")
+    x = os.listdir(prefix)
+    print('os.listdir(~/): ' + str(x))
+
+    target = prefix + "twenty-something_link_spotify"
+    command = "touch " + target
+
+    os.system(command)
+
+    
+    
