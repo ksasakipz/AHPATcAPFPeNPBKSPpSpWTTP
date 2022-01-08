b60225d023387bc88613b74b75b7ff0ac238c2ac
ksasakipz <kevinmsasaki@gmail.com>
2022-01-07 21:47:16 -0800

    2022-01-07 21:47:16.597651

--- 9.py
+++ 9.py
@@ -6,7 +6,7 @@ prefix = "/Users/kevinsasaki/sandbox"
 
 if __name__ == "__main__":
     while True:
-        retval = "Hello to your Macbook pro"= 
+        retval = "Hello to your Macbook pro"
         
         print(f'retval: {retval}')
         
