3e3d51e452c6923b5528a54024b0b2a9057fb158
ksasakipz <kevinmsasaki@gmail.com>
2022-01-08 20:55:20 -0800

    2022-01-08 20:55:20.425814

--- 9.py
+++ 9.py
@@ -21,7 +21,7 @@ if __name__ == "__main__":
         print(f'{eleven}')
 
         out_file = open("garbage", "wb")
-        out_file.write(eleven.to_bytes())
+        out_file.write(eleven.to_bytes(len(str(eleven)))
 
         seed += 1
         
